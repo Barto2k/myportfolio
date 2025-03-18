2c32211ba7d9fb614cc114b61ec92e119e375be2
Marco <marcofiguero3@gmail.com>
2025-03-18 15:06:51 -0300

    cambio proyectos

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -10,54 +10,6 @@ export const Projects = () => {
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                         <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition">
                             <h3 className="text-xl font-bold mb-2">
-                                Zona Fit GYM ABM
-                            </h3>
-                            <p className="text-gray-400 mb-4">
-                                An desktop application to manage a gym using CRUD. Did it for educational purposes following patterns like MVC, DAO, DTO, etc.
-                            </p>
-                            <div className="flex flex-wrap gap-2 mb-4">
-                                {["Spring", "Swing", "JAVA"].map((tech, key) => (
-                                    <span
-                                        key={key}
-                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-                                        {tech}
-                                    </span>
-                                ))}
-                            </div>
-
-                            <div className="flex justify-between items-center">
-                                <a href="https://github.com/Barto2k/ZonaFit-Spring-Swing"  className="text-blue-400 hover:text-blue300 transition-colors my-4">
-                                    ➡️ View Project ⬅️
-                                </a>
-                            </div>
-                        </div>
-
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition">
-                            <h3 className="text-xl font-bold mb-2">
-                                ABM ZonaFitGym
-                            </h3>
-                            <p className="text-gray-400 mb-4">
-                                Using Spring i developed this application for practice.
-                            </p>
-                            <div className="flex flex-wrap gap-2 mb-4">
-                                {["Spring", "Java"].map((tech, key) => (
-                                    <span
-                                        key={key}
-                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-                                        {tech}
-                                    </span>
-                                ))}
-                            </div>
-
-                            <div className="flex justify-between items-center">
-                                <a href="https://github.com/Barto2k/ZonaFitGYM-Spring"  className="text-blue-400 hover:text-blue300 transition-colors my-4">
-                                    ➡️ View Project ⬅️
-                                </a>
-                            </div>
-                        </div>
-
-                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition">
-                            <h3 className="text-xl font-bold mb-2">
                                 ABM of Test Drive Cars
                             </h3>
                             <p className="text-gray-400 mb-4">
@@ -82,14 +34,16 @@ export const Projects = () => {
 
                         <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition">
                             <h3 className="text-xl font-bold mb-2">
-                                ABM of a Medical Clinic
+                                CIMA - ABM
                             </h3>
                             <p className="text-gray-400 mb-4">
-                                An application to manage a medical clinic with REST API.
-                                Made for educational purposes. (Unfinished)
+                                An application to manage a integration  center for people whit disabilities with REST API.
+                                Made for a real client, it contains a patient ABM, another for professionals, logic to change passwords, and logic to log in with authentication using the Bycript library.
+                                To log in to the application you must use the <span className="font-bold"> username: admin and the password: 123 </span> . It should be noted that this software is a demo, therefore you are allowed to upload both patients and professionals. I strongly urge you to do so in order to understand the system and not overload the free server on which it was deployed. The real software has more features (appointment calendar, specialties, etc.) but for privacy reasons with the company I cannot publish them.
+
                             </p>
                             <div className="flex flex-wrap gap-2 mb-4">
-                                {["React", "NodeJS", "ExpressJS", "JavaScript"].map((tech, key) => (
+                                {["React", "NodeJS", "ExpressJS", "JavaScript", "Tailwind CSS", "PostgreSQL"].map((tech, key) => (
                                     <span
                                         key={key}
                                         className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
@@ -99,7 +53,7 @@ export const Projects = () => {
                             </div>
 
                             <div className="flex justify-between items-center">
-                                <a href="https://github.com/Barto2k/CIMA_SOFTWARE" className="text-blue-400 hover:text-blue300 transition-colors my-4">
+                                <a href="https://frontend-cima.onrender.com/" className="text-blue-400 hover:text-blue300 transition-colors my-4">
                                     ➡️ View Project ⬅️
                                 </a>
                             </div>
